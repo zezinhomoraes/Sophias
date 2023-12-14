90437ecf871bf17434e54ff7b11785bfe16d9038
zezinhomoraes <150301566+zezinhomoraes@users.noreply.github.com>
2023-12-14 04:52:44 -0300

    Add files via upload
    pagina inicial e home

---
+++ pagina-inicial.js
@@ -0,0 +1,26 @@
+let time = 4000
+let currentImageIndex = 0
+let images = document.querySelectorAll("#mundos img")
+let max = images.length
+
+function nextImage() {
+
+images[currentImageIndex].classList.remove("selected")
+
+ currentImageIndex++
+  if(currentImageIndex >= 7)
+  currentImageIndex = 0
+  images[currentImageIndex].classList.add("selected")
+    
+   
+}
+function start() {
+    setInterval(() => {
+        nextImage()
+    },time)
+        
+        
+}
+
+window.addEventListener("load", start)
+    
