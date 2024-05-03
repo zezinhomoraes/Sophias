d6739d44809f9bf2b9075bec20560357b7a50828
José Morais <150301566+zezinhomoraes@users.noreply.github.com>
2024-05-02 23:51:13 -0300

    Mitologia grega
    Codigo do botão de busca atualizado e melhorado agora com try/cath para mostar na tela o resultado da busca

--- Grecia/Deuses-gregos/deuses-gregos.js
+++ Grecia/Deuses-gregos/deuses-gregos.js
@@ -13,43 +13,49 @@ abrirMenu.addEventListener('click', (evt)=>{
 // PROGRAMANDO BARRA DE BUSCA.........
 
 // BUSCA NAO ENCONTRADA
-const messageError = document.querySelector(".conteiner")
+const conteiner = document.querySelector(".conteiner")
 const message = document.createElement("h1")
 message.innerHTML="OPS! NÃO ENCONTRADO.</BR>VERIFIQUE OS ACENTOS."
 message.style.cssText="color: #fff; font-size: 30px;"
 
 // REALIZANDO BUSCA
+
 const pesquisa = document.getElementById("iPesquisa")
 const busca = document.querySelector("#icone")
 const conteudo_naoSelect = [...document.querySelectorAll(".conteudo")]
+const conteudo_select = [...document.querySelectorAll(".conteudo h3")]
+
 busca.addEventListener('click',(evt)=>{
-  conteudo_naoSelect.map((e)=>{e.classList.add('nao_pesquisado')})
-  let valorError = [] 
-  const conteudo_select = [...document.querySelectorAll(".conteudo h3")]
-  conteudo_select.find((e)=>{
-    const valorPesquisado= pesquisa.value.toLowerCase()
-    const valorEncontrado =  e.innerHTML.toLowerCase()
-    if(valorPesquisado === valorEncontrado ){
-      messageError.appendChild(message)
-      e.parentElement.classList.remove("nao_pesquisado")
-      messageError.removeChild(message)
-    }else{
-     valorError.push(e)
+  conteudo_naoSelect.map((e)=>{e.classList.add("nao_pesquisado")})
+  const resultadoBusca = conteudo_select.find((e)=>{
+    const pR = pesquisa.value.toLowerCase()
+    const pE = e.innerHTML.toLowerCase()
+    if(pR === pE){
+      return e
     }
   })
-  if(valorError.length == 11){
-    messageError.appendChild(message)
+  
+  try{
+    resultadoBusca.parentElement.classList.remove("nao_pesquisado")
+    conteiner.removeChild(message)
+  }catch(error){
+    if(!(resultadoBusca)){
+      conteiner.appendChild(message)
+    }
   }
 })
+
+
 
 pesquisa.addEventListener("input", (evt)=>{ 
   conteudo_naoSelect.map((e)=>{
     if(pesquisa.value.length === 0){
       e.classList.remove('nao_pesquisado')
+      try{
+        conteiner.removeChild(message)
+      }catch(error){
+
+      }
     }
   })
-  if(pesquisa.value.length === 0){
-    messageError.appendChild(message)
-    messageError.removeChild(message)
-  }
 })
